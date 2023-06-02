dbbf975fef06d986a05c2033dca5311faaaabced
satojun3 <j.sato.kuma@gmail.com>
2023-06-03 00:52:25 +0900

    POSTリクエスト追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, FormEvent, ChangeEvent } from 'react';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8080/api/hello')
@@ -9,9 +10,36 @@ function App() {
       .then(data => setMessage(data.message));
   }, []);
 
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch('http://localhost:8080/api/hello', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ userMessage: inputValue })
+    })
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
   return (
     <div className="App">
       <h1>{message}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Type your message here"
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,15 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello World!' });
+  res.json({ message: 'Hello World!!!' });
+});
+
+app.post('/api/hello', (req, res) => {
+  const userMessage = req.body.userMessage;
+  res.json({ message: `You said: ${userMessage}` });
 });
 
 app.listen(8080, () => {
