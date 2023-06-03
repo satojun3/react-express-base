022dcae1e6ce8b08655bf516a2f1720deca19b80
satojun3 <j.sato.kuma@gmail.com>
2023-06-03 20:25:56 +0900

    レイアウト仮 作成

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,109 @@
-import React, { useEffect, useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+import './index.css';
+
+interface Message {
+  sender: 'user' | 'llmm';
+  content: string;
+}
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
   const [inputValue, setInputValue] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:8080/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message));
-  }, []);
-
+  const [system, setSystem] = useState('');
+  const [properties, setProperties] = useState('');
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const userMessage: Message = { sender: 'user', content: inputValue };
+    setMessages((messages) => [...messages, userMessage]);
+    setInputValue('');
+
     fetch('http://localhost:8080/api/hello', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userMessage: inputValue })
+      body: JSON.stringify({
+        userMessage: inputValue,
+        system: system,
+        properties: properties,
+      }),
     })
-      .then(response => response.json())
-      .then(data => setMessage(data.message));
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        const llmmMessage: Message = { sender: 'llmm', content: data.message };
+        setMessages((messages) => [...messages, llmmMessage]);
+      });
+  };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-  }
+  };
 
   return (
-    <div className="App">
-      <h1>{message}</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleChange}
-          placeholder="Type your message here"
-        />
-        <button type="submit">Send</button>
-      </form>
+    <div className="App bg-gray-100 flex items-center justify-center">
+      <div className="bg-white w-2/3 p-8 shadow-lg h-screen">
+        <h1 className="text-3xl font-semibold mb-6">Chat with LLMM</h1>
+        <form onSubmit={handleSubmit} className="mb-4">
+          <div className="flex mb-2">
+            <input
+              className="flex-grow p-2 mr-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
+              type="text"
+              value={inputValue}
+              onChange={handleChange}
+              placeholder="Type your message here"
+            />
+            <button
+              className="bg-blue-500 text-white py-2 px-4 rounded-md focus:outline-none"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
+          <div className="flex mb-2">
+            <label htmlFor="system" className="w-1/3 font-semibold">
+              System:
+            </label>
+            <input
+              id="system"
+              className="flex-grow p-2 ml-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
+              type="text"
+              value={system}
+              onChange={(e) => setSystem(e.target.value)}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="properties" className="w-1/3 font-semibold">
+              Properties:
+            </label>
+            <input
+              id="properties"
+              className="flex-grow p-2 ml-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
+              type="text"
+              value={properties}
+              onChange={(e) => setProperties(e.target.value)}
+            />
+          </div>
+        </form>
+        <div className="messages overflow-y-auto h-64 mb-4 p-4 bg-gray-200 rounded h-5/6">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-2 ${
+                message.sender === 'user' ? 'text-right' : ''
+              }`}
+            >
+              <span
+                className={`inline-block p-2 rounded-lg ${
+                  message.sender === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-300'
+                }`}
+              >
+                {message.content}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
